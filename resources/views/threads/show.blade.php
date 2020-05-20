53da88d923014a7e6c530a71b820ca7c72ecf068
marti19686 <marti19686@gmail.com>
2020-05-20 23:44:25 +0200

    add reply dorm

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -24,5 +24,25 @@
                 @endforeach
             </div>
         </div>
+
+        @auth
+
+        <div class="row justify-content-center mt-4">
+            <div class="col-md-8">
+                <form  method="POST" action="{{ $thread->path(). '/replies' }}">
+                    @csrf
+                    <div class="form-body mb-3">
+                        <textarea name="body" id="body" class="form-control" placeholder="Have something to say?" rows="5"></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-dark">Post</button>
+                </form>
+            </div>
+        </div>
+        @endauth
+
+        @guest
+            <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion. </p>
+        @endguest
     </div>
 @endsection
