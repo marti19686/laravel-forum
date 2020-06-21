c44a1fa781bab3d23403f722c9d8673d6d372137
marti19686 <marti19686@gmail.com>
2020-06-21 23:09:56 +0200

    UI for favorites reply

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,8 +1,22 @@
 <div class="card mt-3">
     <div class="card-header">
-        <a href="#">
-            {{$reply->owner->name}}
-        </a> said {{ $reply->created_at->diffForHumans() }}...
+        <div class="level">
+            <h6 class="flex">
+                <a href="#">
+                    {{$reply->owner->name}}
+                </a> said {{ $reply->created_at->diffForHumans() }}...
+            </h6>
+
+            <div>
+                <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+                    @csrf
+
+                    <button type="submit" class="btn btn-outline-secondary" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                        {{ $reply->favorites()->count() }} {{ Str::plural('Favorite', $reply->favorites()->count()) }}
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
 
     <div class="card-body">
