0172e73d1cd8a452a75aac32501ae4a4634957a0
marti19686 <marti19686@gmail.com>
2020-10-04 16:29:11 +0200

    event listener for new reply

--- app/Thread.php
+++ app/Thread.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Events\ThreadHasNewReply;
 use App\Notifications\ThreadWasUpdated;
 use Illuminate\Database\Eloquent\Model;
 
@@ -52,12 +53,17 @@ class Thread extends Model
     {
         $reply =  $this->replies()->create($reply);
 
+        event(new ThreadHasNewReply($this, $reply));
+
+        return $reply;
+    }
+
+    public function notifySubscribers($reply)
+    {
         $this->subscriptions
             ->where('user_id', '!=', $reply->user_id)
             ->each
             ->notify($reply);
-
-        return $reply;
     }
 
     public function scopeFilter($query, $filters)
